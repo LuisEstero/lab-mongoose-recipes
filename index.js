c0cd56ecdcf420e31d32191e9616e9303e747686
LuisEstero <104272882+LuisEstero@users.noreply.github.com>
2022-06-23 17:36:56 +0200

    Iteración 1

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+
+
+ 
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,54 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+   title: {
+    type: String,
+    required: true, // este elemento es obligatorio, si no, error!
+    unique: true // este elemento no puede repetirse, si se fuese a repetir, error!
+  },
+
+  level: {
+    type: String,
+    enum : ['Easy Peasy','Amateur Chef','UltraPro Chef'],
+    default: 'NEW'
+  },
+
+  ingredients: {
+    type: [String]
+    
+  },
+
+  cuisine :{
+    type : String,
+    required: true,
+
+  },
+
+  dishType :{
+    type: String,
+    enum : ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert or other'],
+
+  },
+  
+   image : {
+    type : String,
+
+  },
+
+  duration : {
+    Type : Number,
+  },
+ 
+  creator : {
+    type: String,
+  },
+
+  created: {
+    type: Date,
+  }
+  
 });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
