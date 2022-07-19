d96019d712556cbc7af5b78d9fff2e486338f9a1
LuisEstero <luisestero@gmail.com>
2022-07-19 11:57:39 +0200

    Lab 11

--- index.js
+++ index.js
@@ -1,60 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-const myReceta = {
-  title: "Receta Luis",
-  level: "Amateur Chef",
-  ingredients: [
-    "1/2 cup rice vinegar",
-    "5 tablespoons honey",
-    "1/3 cup soy sauce (such as Silver Swan®)",
-    "1/4 cup Asian (toasted) sesame oil",
-    "3 tablespoons Asian chili garlic sauce",
-    "3 tablespoons minced garlic",
-    "salt to taste",
-    "8 skinless, boneless chicken thighs"
-  ],
-  cuisine: "Asian",
-  dishType: "main_course",
-  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-  duration: 40,
-  creator: "Chef LePapu"
-}
-
-
+let newRecipe = {
+  title: "miXto quente",
+  level: "Easy Peasy",
+  ingredients: ["pão francês", "queijo", "presunto"],
+  cuisine: "Brasileira",
+  dishType: "snack",
+  image:
+    "http://culinaria.culturamix.com/blog/wp-content/gallery/misto-quente-3/Misto-Quente-6.jpg",
+  duration: 5,
+  creator: "JOC",
+};
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // Before adding any documents to the database, let's delete all previous entries
+    return Recipe.deleteMany();
   })
   .then(() => {
-
-
-    // actualizar
-    return Recipe.create(myReceta );
+    // iteration 2. newRecipe will be created in our DB
+    return Recipe.create(newRecipe) // returning the promise allows us to resolve it in the next .then(), instead of nested promises.
   })
-
-  .then(()=> {
-   return Recipe.insertMany(data);
+  .then((result) => {
+    console.log(`recipe added: ${result.title}`) // a console.log to verify previous promise resolved correctly
+    // iteration 3. all recipes in data will be inserted in our DB
+    return Recipe.insertMany(data)
   })
-  
-
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
-
-
-
-
-
- 
+  .then((result) => {
+    result.forEach((item) => console.log(`recipe for ${item.title} inserted successfully`)); // a console.log to verify previous promise resolved correctly
+    // iteration 4. To find a recipe by its title and update one attribute
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new:true })
+  })
+  .then((result) => {
+    console.log(`The recipe ${result.title} is updated`) // a console.log to verify previous promise resolved correctly
+    // iteration 5. To find a recipe by its title and delete it
+    return Recipe.findOneAndDelete({ title: "Carrot Cake" })
+  })
+  .then((result) => {
+    console.log(`The recipe ${result.title} is deleted`) // a console.log to verify previous promise resolved correctly
+    // iteration 6. To close the DB.
+    return mongoose.connection.close()
+  })
+  .then(() => console.log(`connection closed`)) // a console.log to verify previous promise resolved correctly
+  .catch((err) => console.log(`an error has occurred: ${err}`)); // to catch errors in promises
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,54 +2,38 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-   title: {
+  title: {
     type: String,
-    required: true, // este elemento es obligatorio, si no, error!
-    unique: true // este elemento no puede repetirse, si se fuese a repetir, error!
+    required: true,
+    //unique: true
   },
-
   level: {
     type: String,
-    enum : ['Easy Peasy','Amateur Chef','UltraPro Chef'],
-    default: 'NEW'
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
   },
-
-  ingredients: {
-    type: [String]
-    
-  },
-
-  cuisine :{
-    type : String,
+  ingredients: [ String ],
+  cuisine: {
+    type: String,
     required: true,
-
   },
-
-  dishType :{
+  dishType: {
     type: String,
-    enum : ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert' , 'other'],
-
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
   },
-  
-   image : {
-    type : String,
-
-  },
-
-  duration : {
-    Type : Number,
-  },
- 
-  creator : {
+  image: {
     type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
   },
-
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
   created: {
     type: Date,
+    default: Date.now
   }
-  
 });
-
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
